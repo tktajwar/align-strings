c5c1647aa63a37d074f1da2cba385f02d00b950d
tajwar <tajwar.earth@proton.me>
2024-01-22 18:02:40 +0600

    add loops to indent each string and print them

--- align_strings.c
+++ align_strings.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
 	char align_ch = -1; // alignment character
+	int indentation_point = 0; // the largest index of alignment character for each string
 
 	/* Find the character to align to */
 	for (int i = 0; argv[1][i]; i++) {                 // for each character of the first string
@@ -9,6 +12,8 @@ int main(int argc, char *argv[])
 			for (int k = 0; argv[j][k]; k++) { // for each character of that string
 				if (argv[j][k] == argv[1][i]) {
 					matches++;
+					if (indentation_point < k)
+						indentation_point = k;
 					break;
 				}
 			}
@@ -20,6 +25,15 @@ int main(int argc, char *argv[])
 	}
 	if (align_ch < 0)
 		return 1;
+
+	for (int i = 1; i < argc; i++) {                              // for each string
+		int align_ch_index = 0; // index of alignment character for this string
+		for (int j = 0; argv[1][align_ch] != argv[i][j] && argv[i][j]; j++) // for each character
+			align_ch_index++;
+		for (int j = 0; j < (indentation_point - align_ch_index); j++)      // for each space required
+			printf(" ");
+		printf("%s\n", argv[i]);
+	}
 
 	return 0;
 }
